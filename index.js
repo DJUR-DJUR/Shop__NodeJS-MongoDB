9ad848cee9cea12353d2029b1ed2f2cf7ecb8239
DJUR-DJUR <djurodjur@gmail.com>
2022-08-02 19:45:46 +0300

    courses model, form

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 
-//настройка handlebasr
+const app = express()
+
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
-app.engine('hbs',hbs.engine)
+
+app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
-//------------------
 
 app.use(express.static('public'))
-app.use('/',homeRoutes)
-app.use('/add',addRoutes)
-app.use('/courses',coursesRoutes)
+app.use(express.urlencoded({extended: true}))
 
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
