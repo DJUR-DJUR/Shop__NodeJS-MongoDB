ab07331df60a1ec3c036f2634d237257a5bfbabe
DJUR-DJUR <djurodjur@gmail.com>
2022-08-03 13:17:19 +0300

    Card DONE

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
-const cardRoutes = require('./routes/card')
 
 const app = express()
 
@@ -17,7 +17,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
