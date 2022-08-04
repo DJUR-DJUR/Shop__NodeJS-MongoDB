a2bd663c31a63b27571277c9ff7c2688f678443f
DJUR-DJUR <djurodjur@gmail.com>
2022-08-04 18:30:49 +0300

    create Login page

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cartRoutes = require('./routes/cart')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const ordersRoutes = require('./routes/orders')
+const authRoutes = require('./routes/auth')
 const User = require('./models/user')
 
 const Handlebars = require('handlebars')
@@ -43,6 +44,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/cart', cartRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000
 
